d58aeebfa22f6b47ecfb4f18ce9185075a031127
Alik Welyn <alikwelynn@gmail.com>
2023-02-19 20:19:47 -0300

    added footer

--- wp-content/themes/saltbox/footer.php
+++ wp-content/themes/saltbox/footer.php
@@ -1,3 +1,69 @@
+    <footer>
+        <div class="footer-upper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6">
+                        <ul class="styled-icons">
+                            <li>Follow us on</li>
+                            <li><a href="" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <ul class="mail">
+                            <li><i class="fa fa-envelope" aria-hidden="true"></i><a href="" target="_blank">support@saltbox.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/saltbox-logo-footer.svg" alt="<?php echo wp_get_document_title(); ?>" class="img-fluid">
+                    </div>
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Saltbox</h4>
+                        <ul>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Partners</a></li>
+                            <li><a href="#">Brokers</a></li>
+                            <li><a href="#">Referrals</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Products</h4>
+                        <ul>
+                            <li><a href="#">Workspaces</a></li>
+                            <li><a href="#">Logistics Services</a></li>
+                            <li><a href="#">Fulfillment</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Locations</h4>
+                        <ul>
+                            <li><a href="#">Atlanta</a></li>
+                            <li><a href="#">Dallas</a></li>
+                            <li><a href="#">Seattle</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Resources</h4>
+                        <ul>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Member Stories</a></li>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 <?php wp_footer(); ?>
 </body>
 </html>
